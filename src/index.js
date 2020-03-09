2c03bfbc8a171b45d204e4c11635d55ac977f97a
Alexis Flores <alex2chivas@hotmail.com>
2020-03-08 19:50:33 -0700

    added class component and some content

--- src/index.js
+++ src/index.js
@@ -1,15 +1,47 @@
-import React from 'react';
+import React, {
+  Component
+} from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-  return ( <
-    div >
-    Alexis Flores <
-    /div>
-  );
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      lat: null
+    };
+
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const {
+          latitude,
+          longitude
+        } = position.coords
+
+        this.setState({
+          lat: latitude
+        })
+      },
+      (err) => console.log(err)
+    );
+  };
+
+
+
+  render() {
+
+    return ( <
+      div >
+      Alexis Flores {
+        this.state.lat
+      } <
+      /div>
+    );
+  };
 };
 
 ReactDOM.render( <
   App / > ,
   document.querySelector("#root")
-)
+);
